da6b3d51df3035b5a7bc2322b96d3de0fea986dd
tylerGoulding <tgouldin@andrew.cmu.edu>
2018-04-29 09:43:42 -0400

    some code.

--- calc_features.py
+++ calc_features.py
@@ -13,6 +13,10 @@ import ast
 from sklearn.svm import SVC
 from collections import Counter 
 
+KNN_POSSIBLE_VERSIONS = ["all","distributed","centralized"]
+
+KNN_VERSION = "centralized"
+
 
 #### facilitate programming between Project Memebers
 dirname_tyler = "/Users/Tyler/Documents/GitHub/Bluetrack/"
@@ -158,7 +162,16 @@ def main():
   global X,Y,testX,testRoots
   rawData = {}
   regions = [];
-  data_folders = ["data4/"] #, "data4/"];
+  if KNN_VERSION not in KNN_POSSIBLE_VERSIONS:
+    print "please used valid version type"
+    exit()
+  if KNN_VERSION == "all":
+    data_folders = ["data3/","data4/"] #, "data4/"];
+  if KNN_VERSION == "centralized":
+    data_folders = ["data4/"] #, "data4/"];
+  if KNN_VERSION == "distributed":
+    data_folders = ["data3/"] #, "data4/"];
+
   for folder in data_folders:
     new_dirname = dirname + folder;
     for filename in os.listdir(new_dirname):
@@ -186,12 +199,12 @@ def main():
             rawData[root].append(raw_list)     
 
   clf5300,clf5302,clf5304 = generate_room_specific_classifiers(rawData);   
-  joblib.dump(clf5300, 'knn_region_given_5300_centralized.pkl') 
-  joblib.dump(clf5302, 'knn_region_given_5302_centralized.pkl') 
-  joblib.dump(clf5304, 'knn_region_given_5304_centralized.pkl') 
+  joblib.dump(clf5300, str.format('knn_region_given_5300_{}.pkl',KNN_VERSION)) 
+  joblib.dump(clf5302, str.format('knn_region_given_5302_{}.pkl',KNN_VERSION)) 
+  joblib.dump(clf5304, str.format('knn_region_given_5304_{}.pkl',KNN_VERSION)) 
 
   ########
-  ## Testing on centralized 11 regions
+  ## Testing on {} 11 regions
   ########
 
   X, Y, testX, testY,_ = generateSets(rawData);
@@ -200,7 +213,7 @@ def main():
   # clf = MLPClassifier(solver='lbfgs', alpha=1e-5,hidden_layer_sizes=(11,4), random_state=1)
   clf = KNeighborsClassifier(n_neighbors=60, weights="distance")
   clf.fit(train_set, Y)
-  joblib.dump(clf, 'knn_region_centralized.pkl') 
+  joblib.dump(clf, str.format('knn_region_{}.pkl',KNN_VERSION) )
 
   predictedTest = clf.predict(testX)
   print "knn - region"
@@ -249,7 +262,7 @@ def main():
 
   clf = KNeighborsClassifier(n_neighbors=12, weights="distance")
   clf.fit(train_set, Y)
-  joblib.dump(clf, 'knn_room_centralized.pkl') 
+  joblib.dump(clf, str.format('knn_room_{}.pkl',KNN_VERSION)) 
 
   predictedTest = clf.predict(testX)
   print "knn - room"
@@ -309,7 +322,7 @@ def main():
   # print node_off_list
   for i,values in enumerate(node_off_list):
     clf,testX,testY = values[:];
-    joblib.dump(clf, str.format('knn_region_node_{}_off_centralized.pkl',i)); 
+    joblib.dump(clf, str.format('knn_region_node_{}_off_{}.pkl',i,KNN_VERSION)); 
     print "Turning off node:", i;
     print clf.score(testX,testY)
 
@@ -317,15 +330,15 @@ def main():
   # print node_off_list
   for i,values in enumerate(node_off_list):
     clf,testX,testY = values[:];
-    joblib.dump(clf, str.format('knn_room_node_{}_off_centralized.pkl',i)); 
+    joblib.dump(clf, str.format('knn_room_node_{}_off_{}.pkl',i,KNN_VERSION)); 
     print "Turning off node:", i;
     print clf.score(testX,testY)
 
   for i in xrange(6):
     clf5300,clf5302,clf5304 = generate_room_specific_classifiers(rawData,i);   
-    joblib.dump(clf5300, str.format('knn_region_given_5300_node_{}_off_centralized.pkl',i)) 
-    joblib.dump(clf5302, str.format('knn_region_given_5302_node_{}_off_centralized.pkl',i)) 
-    joblib.dump(clf5304, str.format('knn_region_given_5304_node_{}_off_centralized.pkl',i)) 
+    joblib.dump(clf5300, str.format('knn_region_given_5300_node_{}_off_{}.pkl',i,KNN_VERSION)) 
+    joblib.dump(clf5302, str.format('knn_region_given_5302_node_{}_off_{}.pkl',i,KNN_VERSION)) 
+    joblib.dump(clf5304, str.format('knn_region_given_5304_node_{}_off_{}.pkl',i,KNN_VERSION)) 
 
   X, Y, testX, testY,_ = generateSets_blah(rawData,'room');
   train_set = np.array(X)
@@ -333,7 +346,7 @@ def main():
   test_setY = np.array(testY)
   clf_node0_node3_off = KNeighborsClassifier(n_neighbors=12, weights="distance").fit(train_set, Y); #SVC(kernel='linear', C=2).fit(train_set, Y)
   print clf_node0_node3_off.score(testX,testY)
-  joblib.dump(clf, 'knn_n0_n3_off_centralized.pkl') 
+  joblib.dump(clf, str.format('knn_n0_n3_off_{}.pkl',KNN_VERSION))
 
 if __name__ == '__main__':
   main()
