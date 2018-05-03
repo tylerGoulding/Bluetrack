19d20ac6c9d13e6480da0e59494f2374aeb090c7
tylerGoulding <tgouldin@andrew.cmu.edu>
2018-05-02 20:32:51 -0400

    removing pickls adding a gitignore changing some code

--- tk2server.py
+++ tk2server.py
@@ -3,6 +3,7 @@
 # I was not sure how we want to do it
 # also there is some unhandled error that occurs randomly that I can't deal with/ignored
 #####################################################################
+from twisted.internet import task
 
 from Tkinter import *
 from twisted.internet import tksupport, reactor
@@ -19,7 +20,7 @@ import random
 import time
 from collections import deque
 from colorutils import Color
-
+import time
 H_5300 = 96
 H_5302 = 325
 W_5302 = 230
@@ -32,13 +33,13 @@ wean5300_positions = [None,None,None]
 wean5302_positions = [None,None,None,None]
 wean5304_positions = [None,None,None,None]
 positions = [None,None,None,None,None,None,None,None,None,None,None]
-wean5300_names = ['5300_lower','5300_middle','5300_upper']
-wean5302_names = ['5302_lower_right','5302_lower_left','5302_upper_right','5302_upper_left']
-wean5304_names = ['5304_lower_right','5304_lower_left','5304_upper_right','5304_upper_left']
-names = wean5300_names + wean5302_names + wean5304_names
+wean5300_region_names = ['5300_lower','5300_middle','5300_upper']
+wean5302_region_names = ['5302_lower_right','5302_lower_left','5302_upper_right','5302_upper_left']
+wean5304_region_names = ['5304_lower_right','5304_lower_left','5304_upper_right','5304_upper_left']
+region_names = wean5300_region_names + wean5302_region_names + wean5304_region_names
 #user_colors = [Color(255,42,26), Color(16,63,251)];
 
-#wean5304_names = ['5304_lower_left','5304_lower_right', '5304_upper_left','5304_upper_right']
+#wean5304_region_names = ['5304_lower_left','5304_lower_right', '5304_upper_left','5304_upper_right']
 
 #load all training models with one node off as well
 # clf_room_all = joblib.load('knn_room_all.pkl')
@@ -77,6 +78,7 @@ names = wean5300_names + wean5302_names + wean5304_names
 class RoomLocator(LineReceiver):
     delimiter = '\n'
     def __init__(self):
+        self.pUserId = -1;
         self.pred = [["","",""], ["","",""]]
         self.pred_d = [["","",""], ["","",""]]
         self.pred_c = [["","",""], ["","",""]]
@@ -133,6 +135,22 @@ class RoomLocator(LineReceiver):
         # self.canvas = self.factory.canvas(root)
         # canvas.pack()
         self.blacklist_dict = create_blacklist();
+    # def connectionLost(self, reason):
+    #     # print "Connection lost yo"
+    #     # print dir(self)
+    #     # i = self.factory.PortUsers.index(self);
+    #     # print i
+    #     print self.pUserId
+    #     if self.pUserId == self.factory.user_list[0].portId:
+    #         print "user 1 is gone"
+    #         self.factory.user_list[0].resetUser();
+    #     elif self.pUserId == self.factory.user_list[1].portId:
+    #         print "user 2 is gone"
+    #         self.factory.user_list[1].resetUser();
+
+        # self.factory.PortUsers.remove(self);
+        # self.facotory.user_list[i].resetUser();
+
 
     def color_blend(self,c1,c2):
         r = (c1.red+c2.red)/2;
@@ -153,10 +171,18 @@ class RoomLocator(LineReceiver):
         return int(user), timestamp, rssi_values
 
     def dataReceived(self, data):
+
         if data != '':
             self.data = self.process_buffer(str(data));
             self.user_id = self.data[0];
-            user = self.factory.user_list[self.user_id]
+            if (self.factory.user_list[self.user_id].portId == None):
+
+                self.factory.user_list[self.user_id].portId == len(self.factory.PortUsers);
+                # print "setting portID"
+                # print self.factory.user_list[self.user_id].portId
+            user = self.factory.user_list[self.user_id];
+            user.lastUpdate = int(round(time.time() * 1000));
+            #print "User{} update: {}".format(self.user_id,user.lastUpdate)
             self.timestamp = self.data[1];
             self.RSSIvalues = self.data[2];
             # print self.RSSIvalues
@@ -195,11 +221,11 @@ class RoomLocator(LineReceiver):
                 # total = self.pred[self.user_id] +self.pred_d[self.user_id] + self.pred_c[self.user_id]
 
             if (self.user_id == 0):
-                print "User Zero:"
-                print "Pred Al:      ", max(self.pred[self.user_id], key=self.pred[self.user_id].count), self.pred[self.user_id]
-                print "Pred Central: ", max(self.pred_c[self.user_id], key=self.pred_c[self.user_id].count), self.pred_c[self.user_id]
-                print "Pred Distrib: ", max(self.pred_d[self.user_id], key=self.pred_d[self.user_id].count), self.pred_d[self.user_id]
-                print "Most Commom:  ", max(total, key=total.count)
+                #print "User Zero:"
+                ##print "Pred Al:      ", max(self.pred[self.user_id], key=self.pred[self.user_id].count), self.pred[self.user_id]
+                #print "Pred Central: ", max(self.pred_c[self.user_id], key=self.pred_c[self.user_id].count), self.pred_c[self.user_id]
+                #print "Pred Distrib: ", max(self.pred_d[self.user_id], key=self.pred_d[self.user_id].count), self.pred_d[self.user_id]
+                print "Most Common:  ", max(total, key=total.count)
                 # if "5302_upper_left" in total:
                     # print "Ignore most common: In 5302_upper_left"
                 print "____________"
@@ -224,56 +250,146 @@ class RoomLocator(LineReceiver):
                 user.previous_region_c = user.current_region_c; 
 
                 # if (self.factory.user_list[0].current_region == self.factory.user_list[1].current_region):
-                #     index = names.index(user.current_region);
+                #     index = region_names.index(user.current_region);
                 #     self.factory.canvas.itemconfig(positions[index], fill='DarkOrchid4');
 
-    def connectionMade(self):
-        print "hello"
-        # self.factory was set by the factory's default buildProtocol:
+    # def connectionMade(self):
 
+        # self.factory.PortUsers +=
+        # print "len of fact: ",len(self.factory.PortUsers)
+        # print "hello"
+        # self.factory was set by the factory's default buildProtocol:
     def updateCanvas(self):
         user1 = self.factory.user_list[0];
         user2 = self.factory.user_list[1];
+        # if not (user1.checkValid()):
+        #     user1.resetUser();
+        # if not (user2.checkValid()):
+        #     user2.resetUser();
         user1_index = -1;
         user2_index = -1;
         user1_start_color = 5 - len(user1.path_q);
         user2_start_color = 5 - len(user2.path_q);
-        print "start color from index", user1_start_color;
-        for location in names:
+
+        #print "start color from index", user1_start_color;
+        # for i in user1.path_q:
+        #     index = region_names.index(i);
+        #     c = positions[index].coords;
+        #     cords.append(c);
+        # # print cords
+        # for rect in positions:
+        #     self.factory.canvas.itemconfig(positions[index], fill='cornsilk2');
+        # u1_path = list(reversed(list(user1.path_q)))
+        # u2_path = list(reversed(list(user1.path_q)))
+        # for location in reversed(list(user1.path_q)):
+            # if 
+            # self.factory.canvas.itemconfig(positions[index], fill=str(new_color.hex));
+
+        for location in region_names:
             if location != "":
-                index = names.index(location);
+                index = region_names.index(location);
                 if ((location in user1.path_q) and (location in user2.path_q)):
-                    user1_index = list(user1.path_q).index(location);
-                    user2_index = list(user2.path_q).index(location);
+                    user1_index = len(list(user1.path_q)) - list(user1.path_q)[::-1].index(location) - 1;
+                    user2_index = len(list(user2.path_q)) - list(user2.path_q)[::-1].index(location) - 1;
                     user1_color = user1.colors[user1_start_color + user1_index];
                     user2_color = user2.colors[user2_start_color + user2_index];
                     new_color = self.color_blend(user1_color, user2_color);
                     self.factory.canvas.itemconfig(positions[index], fill=str(new_color.hex));
                 elif (location in user1.path_q):
-                    user1_index = list(user1.path_q).index(location);
+                    user1_index = len(list(user1.path_q)) - list(user1.path_q)[::-1].index(location) - 1;
+                    #print user1_index, location, user1.path_q
                     user1_color = user1.colors[user1_start_color + user1_index];
                     self.factory.canvas.itemconfig(positions[index], fill=str(user1_color.hex));
                 elif (location in user2.path_q):
-                    user2_index = list(user2.path_q).index(location);
+                    user2_index = len(list(user2.path_q)) - list(user2.path_q)[::-1].index(location) - 1;
                     user2_color = user2.colors[user2_start_color + user2_index];
                     self.factory.canvas.itemconfig(positions[index], fill=str(user2_color.hex));
                 else: 
                     self.factory.canvas.itemconfig(positions[index], fill='cornsilk2');
+
+
 
 class RoomLocatorFactory(Factory):
     # This will be used by the default buildProtocol to create new protocols:
     protocol = RoomLocator
     def __init__(self,canvas, numUsers = None, verbose = False):
         self.canvas = canvas;
+        self.clearing = 0
         self.numUsers = numUsers or 1;
         self.verbose = verbose;
         self.user_color_list = [ [Color(hex='#ff2a1a'),Color(hex='#f66340'),Color(hex='#f09063'),Color(hex='#edb284'),Color(hex='#eccba2')],
                                  [Color(hex='#103ffb'),Color(hex='#38baf3'),Color(hex='#5dedd1'),Color(hex='#7feba0'),Color(hex='#aeea9f')] ]
         self.user_color_list = [self.user_color_list[0][::-1], self.user_color_list[1][::-1]]                   
         self.user_list = [User(0, self.user_color_list[0]), User(1, self.user_color_list[1])];
+        self.PortUsers = []
+        def clearIfNec():
+            #print "!!!!!!!!!!!checking if Valid!!!!!"
+            for user in self.user_list:
+                currentMillis = int(round(time.time() * 1000));
+                if (user.lastUpdate != -1) and (abs(currentMillis-user.lastUpdate) > 5*1000):
+                    user.resetUser();
+                    self.clearing = 1;
+                    #print "clearing!"
+            if self.clearing == 1:
+                print "User 0 is detected off"
+                self.clearing = 0;
+                self.updateCanvas();
+                    # protocol.updateCanvas();
+        self.looping_call = task.LoopingCall(clearIfNec)
+        self.looping_call.start(3)
+    def updateCanvas(self):
+        user1 = self.user_list[0];
+        user2 = self.user_list[1];
+        # if not (user1.checkValid()):
+        #     user1.resetUser();
+        # if not (user2.checkValid()):
+        #     user2.resetUser();
+        user1_index = -1;
+        user2_index = -1;
+        user1_start_color = 5 - len(user1.path_q);
+        user2_start_color = 5 - len(user2.path_q);
+
+        #print "start color from index", user1_start_color;
+        # for i in user1.path_q:
+        #     index = region_names.index(i);
+        #     c = positions[index].coords;
+        #     cords.append(c);
+        # # print cords
+        # for rect in positions:
+        #     self.canvas.itemconfig(positions[index], fill='cornsilk2');
+        # u1_path = list(reversed(list(user1.path_q)))
+        # u2_path = list(reversed(list(user1.path_q)))
+        # for location in reversed(list(user1.path_q)):
+            # if 
+            # self.canvas.itemconfig(positions[index], fill=str(new_color.hex));
+
+        for location in region_names:
+            if location != "":
+                index = region_names.index(location);
+                if ((location in user1.path_q) and (location in user2.path_q)):
+                    user1_index = len(list(user1.path_q)) - list(user1.path_q)[::-1].index(location) - 1;
+                    user2_index = len(list(user2.path_q)) - list(user2.path_q)[::-1].index(location) - 1;
+                    user1_color = user1.colors[user1_start_color + user1_index];
+                    user2_color = user2.colors[user2_start_color + user2_index];
+                    new_color = self.color_blend(user1_color, user2_color);
+                    self.canvas.itemconfig(positions[index], fill=str(new_color.hex));
+                elif (location in user1.path_q):
+                    user1_index = len(list(user1.path_q)) - list(user1.path_q)[::-1].index(location) - 1;
+                    #print user1_index, location, user1.path_q
+                    user1_color = user1.colors[user1_start_color + user1_index];
+                    self.canvas.itemconfig(positions[index], fill=str(user1_color.hex));
+                elif (location in user2.path_q):
+                    user2_index = len(list(user2.path_q)) - list(user2.path_q)[::-1].index(location) - 1;
+                    user2_color = user2.colors[user2_start_color + user2_index];
+                    self.canvas.itemconfig(positions[index], fill=str(user2_color.hex));
+                else: 
+                    self.canvas.itemconfig(positions[index], fill='cornsilk2');
+
 
 class User():
     def __init__(self, number, colors):
+        self.lastUpdate = -1;
+        self.portId = None;
         self.path_q = deque(maxlen=5)
         self.number = number;
         # self.path = []
@@ -288,6 +404,31 @@ class User():
         self.blacklist_count_c = 0;
         self.i = 0;
         self.colors = colors;
+        self.timeoutSeconds = 5;
+    def checkValid(self):
+        currentMillis = int(round(time.time() * 1000));
+        #print "tttslkdfjskldfhaskldjf ",currentMillis-self.lastUpdate
+        if (self.lastUpdate != -1) and (abs(currentMillis-self.lastUpdate) > self.timeoutSeconds*1000):
+            return -1;
+        return 0;
+
+    def resetUser(self):
+        self.lastUpdate = -1;
+
+        self.path_q = deque(maxlen=5)
+        # self.number = number;
+        # self.path = []
+        self.previous_region = ""
+        self.current_region = ""
+        self.previous_region_c = ""
+        self.current_region_c = "" 
+        self.previous_region_d = ""
+        self.current_region_d = "" 
+        self.blacklist_count = 0;
+        self.blacklist_count_d = 0;
+        self.blacklist_count_c = 0;
+        self.i = 0;
+        # self.colors = colors;
 
     def predictRegion(self, rssi_values, blacklist_dict, node_off, clf_room, clf5300, clf5302, 
                       clf5304, clf_node_off, region="all"):
@@ -328,7 +469,7 @@ class User():
             else:
                 if (predicted_region.startswith(blacklist_dict[self.current_region_d]) and (self.blacklist_count_d<3)):
                     self.blacklist_count_d += 1; 
-                    print "blacklisting: " + predicted_region;
+                    #print "blacklisting: " + predicted_region;
                     # do nothing
                 else:
                     self.current_region_d = predicted_region;
@@ -343,7 +484,7 @@ class User():
             else:
                 if (predicted_region.startswith(blacklist_dict[self.current_region_c]) and (self.blacklist_count_c<3)):
                     self.blacklist_count_c += 1; 
-                    print "blacklisting: " + predicted_region;
+                    #print "blacklisting: " + predicted_region;
                     # do nothing
                 else:
                     self.current_region_c = predicted_region;
@@ -353,15 +494,15 @@ class User():
 
     def addPath(self):
         self.path_q.append(self.current_region)
-        print len(self.path_q);
+        #print len(self.path_q);
         # if (self.current_region != self.previous_region):
             # self.path.append(self.current_region);
 
 
     def clearPrevious(self, w):
         if ((len(self.path) > 0)):
-            index = names.index(self.path[-1]);
-            print w.itemcget(positions[index], "fill")
+            index = region_names.index(self.path[-1]);
+            #print w.itemcget(positions[index], "fill")
             if (self.number == 1) and (w.itemcget(positions[index], "fill") == 'DarkOrchid4'):
                 w.itemconfig(positions[index], fill='blue');
             if (self.number == 0) and (w.itemcget(positions[index], "fill") == 'DarkOrchid4'):
@@ -370,7 +511,7 @@ class User():
             w.itemconfig(positions[index], fill='cornsilk2');
 
     def drawCurrent(self,w):
-        index = names.index(self.current_region);
+        index = region_names.index(self.current_region);
         w.itemconfig(positions[index], fill=self.colors[0]);
 
 def drawSpace(w):
@@ -481,7 +622,7 @@ def main():
     w.pack()
     drawSpace(w);
     tksupport.install(root)
-    reactor.listenTCP(8040, RoomLocatorFactory(w))
+    reactor.listenTCP(8001, RoomLocatorFactory(w))
     reactor.run()
 
 if __name__ == '__main__':
